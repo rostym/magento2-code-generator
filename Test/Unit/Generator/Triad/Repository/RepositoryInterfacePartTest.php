a2a061b3746fc4b965f015899889272054095a2b
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-05-10 00:24:27 +0300

    - Added RepositoryInterfacePart test

---
+++ Test/Unit/Generator/Triad/Repository/RepositoryInterfacePartTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of Code Generator for Magento.
+ * (c) 2016. Rostyslav Tymoshenko <krifollk@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Krifollk\CodeGenerator\Test\Unit\Generator\Triad\Repository;
+
+use Krifollk\CodeGenerator\Api\GeneratorResultInterface;
+use Krifollk\CodeGenerator\Model\Generator\Triad\Repository\RepositoryInterfacePart;
+
+/**
+ * Class RepositoryInterfacePartTest
+ *
+ * @package Krifollk\CodeGenerator\Test\Unit\Generator\Triad\Repository
+ */
+class RepositoryInterfacePartTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param array $inputData
+     * @param array $resultData
+     *
+     * @dataProvider dataProvider
+     * @test
+     */
+    public function generate(array $inputData, array $resultData)
+    {
+        $repositoryInterfaceGenerator = $this->getMockBuilder(RepositoryInterfacePart::class)
+            ->setMethods(['getBasePath'])
+            ->setConstructorArgs([
+                'moduleName'         => $inputData['moduleName'],
+                'entityName'         => $inputData['entityName'],
+                'modelInterfaceName' => $inputData['modelInterfaceName'],
+            ])
+            ->getMock();
+
+        $repositoryInterfaceGenerator->method('getBasePath')->willReturn('/base/path');
+
+        /** @var RepositoryInterfacePart $repositoryInterfaceGenerator */
+        $result = $repositoryInterfaceGenerator->generate();
+
+        self::assertInstanceOf(GeneratorResultInterface::class, $result);
+        self::assertEquals($resultData['content'], $result->getContent());
+        self::assertEquals($resultData['entityName'], $result->getEntityName());
+        self::assertEquals($resultData['destinationFile'], $result->getDestinationFile());
+    }
+
+    public function dataProvider()
+    {
+        return require __DIR__ . '/_files/repositoryInterfaceData.php';
+    }
+}
