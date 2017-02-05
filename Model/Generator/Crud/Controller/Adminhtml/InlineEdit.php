0df00790f9f9f204679c62ddee59b586b89196bc
Rostyslav <krifollk@gmail.com>
2017-02-05 21:19:09 +0200

    - Added generators of action

---
+++ Model/Generator/Crud/Controller/Adminhtml/InlineEdit.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Code Generator for Magento.
+ * (c) 2017. Rostyslav Tymoshenko <krifollk@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Krifollk\CodeGenerator\Model\Generator\Crud\Controller\Adminhtml;
+
+use Krifollk\CodeGenerator\Api\GeneratorResultInterface;
+use Krifollk\CodeGenerator\Model\Generator\AbstractGenerator;
+
+/**
+ * Class InlineEdit
+ *
+ * @package Krifollk\CodeGenerator\Model\Generator\Crud\Controller\Adminhtml
+ */
+class InlineEdit extends AbstractGenerator
+{
+    /**
+     * Generate entity
+     *
+     * @return GeneratorResultInterface
+     */
+    public function generate()
+    {
+        // TODO: Implement generate() method.
+    }
+}
